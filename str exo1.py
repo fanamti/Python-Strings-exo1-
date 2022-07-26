30c026a898f0bf45d85d29633ac1cf9bdc54e500
THIERNO IB II DIALLO <67205316+fanamti@users.noreply.github.com>
2022-07-27 02:03:13 +0300

    Add files via upload

---
+++ str exo1.py
@@ -0,0 +1,9 @@
+
+test_str = "This is string for learning"
+
+print("The first symbol of string:", test_str [3])
+print("The last symbol of string:", test_str [26])
+
+print("Extracted word:", test_str[8:14])
+
+print("Total number of symbols in test_str:", len (test_str))
